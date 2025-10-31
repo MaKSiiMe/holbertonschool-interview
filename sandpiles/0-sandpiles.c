8fdebcead16aa6ff2d75b5c6944768616eaa77c1
MaKSiiMe <maxime.truel@gmail.com>
2025-10-31 10:03:36 +0100

    Ajout des fonctions pour gérer les opérations sur les tas de sable, y compris l'impression, la vérification de stabilité et le renversement.

---
+++ sandpiles/0-sandpiles.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+
+/**
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ */
+static void print_grid(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/**
+ * is_stable - Check if sandpile is stable
+ * @grid: 3x3 grid to check
+ *
+ * Return: 1 if stable, 0 if unstable
+ */
+static int is_stable(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (grid[i][j] > 3)
+                return (0);
+        }
+    }
+    return (1);
+}
+
+/**
+ * topple - Perform one toppling round
+ * @grid: 3x3 grid to topple
+ */
+static void topple(int grid[3][3])
+{
+    int temp[3][3];
+    int i, j;
+
+    /* Copy original grid */
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            temp[i][j] = grid[i][j];
+        }
+    }
+
+    /* Topple unstable cells */
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (temp[i][j] > 3)
+            {
+                grid[i][j] -= 4;
+                /* Add to adjacent cells */
+                if (i > 0)
+                    grid[i - 1][j]++;
+                if (i < 2)
+                    grid[i + 1][j]++;
+                if (j > 0)
+                    grid[i][j - 1]++;
+                if (j < 2)
+                    grid[i][j + 1]++;
+            }
+        }
+    }
+}
+
+/**
+ * sandpiles_sum - Computes sum of two sandpiles
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+
+    /* Add grid2 to grid1 */
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            grid1[i][j] += grid2[i][j];
+        }
+    }
+
+    /* Stabilize grid1 */
+    while (!is_stable(grid1))
+    {
+        printf("=\n");
+        print_grid(grid1);
+        topple(grid1);
+    }
+}
