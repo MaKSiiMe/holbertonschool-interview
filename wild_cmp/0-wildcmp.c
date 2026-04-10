3fc3f3625e11be7abdadc595534a2c3c72482f16
MaKSiiMe <51089969+MaKSiiMe@users.noreply.github.com>
2026-04-10 15:12:37 +0200

    Ajout de la fonction skip_stars pour simplifier la gestion des étoiles consécutives dans wildcmp.

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -1,20 +1,24 @@
 #include "holberton.h"
 
+char *skip_stars(char *s2)
+{
+    if (*s2 == '*' && *(s2 + 1) == '*')
+        return (skip_stars(s2 + 1));
+    return (s2);
+}
+
 int wildcmp(char *s1, char *s2)
 {
     if (*s2 == '*')
     {
-        while (*(s2 + 1) == '*')
-            s2++;
+        s2 = skip_stars(s2);
         if (*(s2 + 1) == '\0')
             return (1);
-        while (*s1 != '\0')
-        {
-            if (wildcmp(s1, s2 + 1))
-                return (1);
-            s1++;
-        }
-        return (wildcmp(s1, s2 + 1));
+        if (wildcmp(s1, s2 + 1))
+            return (1);
+        if (*s1 == '\0')
+            return (0);
+        return (wildcmp(s1 + 1, s2));
     }
     if (*s1 == '\0' && *s2 == '\0')
         return (1);
