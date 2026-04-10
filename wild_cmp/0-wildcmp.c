f6a41543fbdb4479030369fb39be4f6da85f31a4
MaKSiiMe <51089969+MaKSiiMe@users.noreply.github.com>
2026-04-10 15:14:19 +0200

    Refactor la fonction skip_stars pour améliorer la lisibilité et la cohérence du code dans wildcmp.

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -1,30 +1,43 @@
 #include "holberton.h"
 
+/**
+ * skip_stars - skip consecutive '*' characters in a pattern string
+ * @s2: pointer to the pattern string
+ *
+ * Return: pointer to the first non-'*' character or the last '*' in a run
+ */
 char *skip_stars(char *s2)
 {
-    if (*s2 == '*' && *(s2 + 1) == '*')
-        return (skip_stars(s2 + 1));
-    return (s2);
+	if (*s2 == '*' && *(s2 + 1) == '*')
+		return (skip_stars(s2 + 1));
+	return (s2);
 }
 
+/**
+ * wildcmp - compare two strings allowing '*' wildcards in s2
+ * @s1: first string to compare
+ * @s2: second string containing wildcards
+ *
+ * Return: 1 if the strings can be considered identical, otherwise 0
+ */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '*')
-    {
-        s2 = skip_stars(s2);
-        if (*(s2 + 1) == '\0')
-            return (1);
-        if (wildcmp(s1, s2 + 1))
-            return (1);
-        if (*s1 == '\0')
-            return (0);
-        return (wildcmp(s1 + 1, s2));
-    }
-    if (*s1 == '\0' && *s2 == '\0')
-        return (1);
-    if (*s1 == '\0' || *s2 == '\0')
-        return (0);
-    if (*s1 == *s2)
-        return (wildcmp(s1 + 1, s2 + 1));
-    return (0);
+	if (*s2 == '*')
+	{
+		s2 = skip_stars(s2);
+		if (*(s2 + 1) == '\0')
+			return (1);
+		if (wildcmp(s1, s2 + 1))
+			return (1);
+		if (*s1 == '\0')
+			return (0);
+		return (wildcmp(s1 + 1, s2));
+	}
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
