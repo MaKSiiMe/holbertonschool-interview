4c0efa78010eb4713c7ca3101fe5cf2779d73b27
MaKSiiMe <51089969+MaKSiiMe@users.noreply.github.com>
2026-04-16 15:51:31 +0200

    Ajout de la fonction is_valid_substring pour vérifier la validité des sous-chaînes concaténées et amélioration de la fonction find_substring pour utiliser cette vérification.

--- substring/substring.c
+++ substring/substring.c
@@ -3,6 +3,54 @@
 #include <string.h>
 
 /**
+ * is_valid_substring - Check if substring at position is valid concatenation
+ * @s: The string
+ * @pos: Position to start checking
+ * @words: Array of words
+ * @nb_words: Number of words
+ * @word_len: Length of each word
+ *
+ * Return: 1 if valid, 0 otherwise
+ */
+static int is_valid_substring(char const *s, int pos, char const **words,
+int nb_words, int word_len)
+{
+int *word_used;
+int i;
+int j;
+int found;
+
+word_used = malloc(nb_words * sizeof(int));
+if (!word_used)
+return (0);
+
+for (i = 0; i < nb_words; i++)
+word_used[i] = 0;
+
+for (i = 0; i < nb_words; i++)
+{
+found = 0;
+for (j = 0; j < nb_words; j++)
+{
+if (!word_used[j] &&
+strncmp(s + pos + i * word_len, words[j], word_len) == 0)
+{
+word_used[j] = 1;
+found = 1;
+break;
+}
+}
+if (!found)
+{
+free(word_used);
+return (0);
+}
+}
+free(word_used);
+return (1);
+}
+
+/**
  * find_substring - Finds all substrings that are concatenations of words
  * @s: The string to scan
  * @words: Array of words that must be concatenated
@@ -13,92 +61,51 @@
  */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-	int word_len;
-	int total_len;
-	int s_len;
-	int *result;
-	int result_count;
-	int i;
-	int j;
-	int k;
-	int *word_used;
-	int found;
-	int valid;
+int word_len;
+int total_len;
+int s_len;
+int *result;
+int result_count;
+int i;
 
-	if (!s || !words || nb_words <= 0 || !n)
-		return (NULL);
+if (!s || !words || nb_words <= 0 || !n)
+return (NULL);
 
-	s_len = strlen(s);
-	word_len = strlen(words[0]);
-	total_len = word_len * nb_words;
+s_len = strlen(s);
+word_len = strlen(words[0]);
+	total_len = word_len *nb_words;
 
-	if (s_len < total_len)
-	{
-		*n = 0;
-		return (NULL);
-	}
+if (s_len < total_len)
+{
+*n = 0;
+return (NULL);
+}
 
-	result = malloc((s_len - total_len + 2) * sizeof(int));
-	if (!result)
-		return (NULL);
+result = malloc((s_len - total_len + 2) * sizeof(int));
+if (!result)
+return (NULL);
 
-	word_used = malloc(nb_words * sizeof(int));
-	if (!word_used)
-	{
-		free(result);
-		return (NULL);
-	}
+result_count = 0;
 
-	result_count = 0;
+/*
+ * For each starting position in the string
+ */
+for (i = 0; i <= s_len - total_len; i++)
+{
+if (is_valid_substring(s, i, words, nb_words, word_len))
+result[result_count++] = i;
+}
 
-	/* For each starting position in the string */
-	for (i = 0; i <= s_len - total_len; i++)
-	{
-		/* Initialize word used array for this position */
-		for (j = 0; j < nb_words; j++)
-			word_used[j] = 0;
+*n = result_count;
 
-		/* Check if substring starting at position i is valid */
-		valid = 1;
-		for (j = 0; j < nb_words; j++)
-		{
-			/* Try to find a matching word for this chunk */
-			found = 0;
-			for (k = 0; k < nb_words; k++)
-			{
-				/* Check if word k hasn't been used and matches this chunk */
-				if (!word_used[k] && 
-					strncmp(s + i + j * word_len, words[k], word_len) == 0)
-				{
-					word_used[k] = 1;
-					found = 1;
-					break;
-				}
-			}
+/*
+ * Return NULL if no valid substrings found
+ */
+if (result_count == 0)
+{
+free(result);
+return (NULL);
+}
 
-			/* If no matching word found for this chunk, position is invalid */
-			if (!found)
-			{
-				valid = 0;
-				break;
-			}
-		}
-
-		/* If all chunks matched, add this position to results */
-		if (valid)
-			result[result_count++] = i;
-	}
-
-	free(word_used);
-
-	*n = result_count;
-
-	/* Return NULL if no valid substrings found */
-	if (result_count == 0)
-	{
-		free(result);
-		return (NULL);
-	}
-
-	return (result);
+return (result);
 }
